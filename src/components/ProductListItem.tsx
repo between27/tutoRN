de06ef67ac0ace4270fa7b4a83537d8874e53f25
Aboubacar <djarimias@gmail.com>
2024-05-23 00:39:59 +0200

    Revert "Reapply "ajout d'un S""
    This reverts commit 0a8c74b0c528894bbf940507e29849b59e693d36.

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -12,7 +12,7 @@ const ProductListItem = ({ product }) => {
         resizeMode="contain"
       />
       <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>$s{product.price.toFixed(2)}</Text>
+      <Text style={styles.price}>${product.price.toFixed(2)}</Text>
     </View>
   );
 };
